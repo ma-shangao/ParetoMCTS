b8e07ed6d5af2d93385332c2b5ea60ed1ec81ba6
MA Song <song.ma.18@ucl.ac.uk>
2024-04-03 01:17:08 +0100

    Fix import statement in hotspots.py

--- demo/hotspots.py
+++ demo/hotspots.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 from pmcts.planners import UCT
-from demo.pgm2numpy import read_pgm
+from pgm2numpy import read_pgm
 
 plt.rcParams["image.origin"] = "lower"
 plt.rcParams["image.cmap"] = "gray_r"
